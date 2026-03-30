675915f4a9a75bd5e2d1876462a5b84b8e465934
vitaliyoliynik <oliynikvs@gmail.com>
2026-03-30 18:55:29 +0200

    pep8

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from typing import Callable
 
 
+
+
 def cache(func: Callable) -> Callable:
     storage = {}
     def wrapper(*args):
@@ -13,6 +15,3 @@ def cache(func: Callable) -> Callable:
             storage[args] = result
             return result
     return wrapper
-
-
-
